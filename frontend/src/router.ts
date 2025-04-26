56e5a8ef1da70e590d24b1d8664e942aa11a5760
Florian Mitter <office@fmw-solutions.at>
2025-04-26 10:13:36 +0200

    add PageNotFoundView

--- frontend/src/router.ts
+++ frontend/src/router.ts
@@ -5,7 +5,7 @@ import ServiceEPU from "@/views/ServiceEPU.vue";
 import ServiceKMU from "@/views/ServiceKMU.vue";
 import ContactView from "@/views/ContactView.vue";
 import ImprintView from "@/views/ImprintView.vue";
-
+import NotFoundView from "@/views/NotFoundView.vue";
 
 const routes = [
   { path: "/", name: "Home", component: HomeView },
@@ -14,6 +14,12 @@ const routes = [
   { path: "/service-kmu", name: "ServiceKMU", component: ServiceKMU },
   { path: "/contact", name: "Contact", component: ContactView },
   { path: "/imprint", name: "Imprint", component: ImprintView },
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'NotFound',
+    component: NotFoundView,
+  }
+  
 
 ];
 
